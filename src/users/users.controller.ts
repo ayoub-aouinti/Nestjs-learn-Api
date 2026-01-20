4965841da663a113155fadb31c94f70939dff5a0
ayoub-aouinti <ayoubaouinti1990@gmail.com>
2026-01-20 22:43:58 +0100

    second commit

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,42 @@
 import { Controller } from '@nestjs/common';
+import { Body } from '@nestjs/common';
+import { Delete } from '@nestjs/common';
+import { Get } from '@nestjs/common';
+import { Param } from '@nestjs/common';
+import { Patch } from '@nestjs/common';
+import { Post } from '@nestjs/common';
+import { Query } from '@nestjs/common';
+
+enum Role {
+  INTERN = 'INTERN',
+  ENGINEER = 'ENGINEER',
+  ADMIN = 'ADMIN',
+}
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get() // GET /users
+  findAll(@Query('role') role?: Role) {
+    return [];
+  }
+
+  @Get(':id') // GET /users/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post() // POST /users
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  @Patch(':id') // PATCH /users/:id
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id') // DELETE /users/:id
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,4 @@
-import { Body, Delete, Get, Module, Param, Patch, Post } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
@@ -6,29 +6,4 @@ import { UsersService } from './users.service';
   controllers: [UsersController],
   providers: [UsersService],
 })
-export class UsersModule {
-  @Get() // GET /users
-  findAll() {
-    return [];
-  }
-
-  @Get(':id') // GET /users/:id
-  findOne(@Param('id') id: string) {
-    return { id };
-  }
-
-  @Post() // POST /users
-  create(@Body() user: { '' }) {
-    return user;
-  }
-
-  @Patch(':id') // PATCH /users/:id
-  update(@Param('id') id: string, @Body() userUpdate: { '' }) {
-    return { id, ...userUpdate };
-  }
-
-  @Delete(':id') // DELETE /users/:id
-  delete(@Param('id') id: string) {
-    return { id };
-  }
-}
+export class UsersModule {}
